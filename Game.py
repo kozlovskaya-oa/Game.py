e8c86a0be269121aebb6c3f4164d4727d56b6b2f
Olga <kozlovskayaoa@gmail.com>
2021-12-25 14:59:37 +0600

    oening(doesnt work)

---
+++ Game.py
@@ -0,0 +1,59 @@
+import pygame
+import sys
+
+
+FPS = 50
+
+def load_image(name, color_key=None):
+    try:
+        fullname = os.path.join(name)
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print(f'В папке отсутствует файл: {name}')
+        raise SystemExit(message)
+
+    if color_key == -1:
+        color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+size = width, height = (700, 300)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Приключения Бобы')
+all_sprites = pygame.sprite.Group()
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def start_screen():
+    intro_text = ["Приключения Бобы", "",
+                  "Преодолевай препятствия и выигрывай",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
